4f2c7880f5be41cdd60344923af6ee0bf5b5f9c0
miozel <99088083+miozel@users.noreply.github.com>
2022-06-19 14:42:55 +0200

    Added Welcome component.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Home.module.scss';
+import Welcome from 'components/Welcome/Welcome'
 
 const Home = () => (
   <div className={styles.Home}>
-    Welcome!!!
+    <Welcome />
   </div>
 );
 
